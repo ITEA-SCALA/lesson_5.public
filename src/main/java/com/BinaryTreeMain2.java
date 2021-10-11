9254e4d4a5cac3bf0e61a0568facdd3053f13122
Lazarchuk <a.lazarchuk@cartsys.com.ua>
2021-10-11 18:46:20 +0300

    #master

--- src/main/java/com/BinaryTreeMain2.java
+++ src/main/java/com/BinaryTreeMain2.java
@@ -20,13 +20,17 @@ class BinaryTreeMain2 {
 
     public static void main(String[] args) {
         Node root = new Node(1);
+
         root.left = new Node(2);
-        root.left.left = new Node(4);
         root.right = new Node(3);
+
+        root.left.left = new Node(4);
         root.left.right = new Node(5);
+
         root.right.left = new Node(6);
-        root.right.left.left = new Node(8);
         root.right.right = new Node(7);
+
+        root.right.left.left = new Node(8);
         root.right.right.right = new Node(9);
 
         root.print();
