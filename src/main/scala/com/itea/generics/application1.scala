4f10739e60cbcf94062425045a39e95d65b83844
Lazarchuk <a.lazarchuk@cartsys.com.ua>
2021-10-14 16:45:27 +0300

    #master lesson-6

--- src/main/scala/com/itea/generics/application1.scala
+++ src/main/scala/com/itea/generics/application1.scala
@@ -5,6 +5,10 @@ package com.itea.generics
  */
 object Application1 extends App {
 
+  /**
+   * @see http://aperiodic.net/phil/scala/s-99/p01.scala
+   *      P01 (*) Находим последний элемент списка.
+   */
   // P01 (*) Find the last element of a list.
 
   val list: List[Int] = List(1, 1, 2, 3, 5, 8)
@@ -22,6 +26,9 @@ object Application1 extends App {
     case h :: Nil => h              //TODO: [здесь инфиксная операция для работы со списком типа 'val l = 1 :: 2 :: 3 :: Nil'] а в случае когда дошли до крайнего элемента 'Nil', тогда в этом случае возвращаем его предшественника
     case _ :: t => lastRecursive(t) //TODO: [здесь инфиксная операция для работы со списком типа 'val l = 1 :: 2 :: 3 :: Nil'] в case получаем какой то список '_'  дальше вызываем метод next '::' следующий элемент списка...  sound because `next` is used only locally
   }
+
+  // нижнее подчеркивание после 'case' (pattern matching) говорит о том что значение будет игнорироваться
+  // но если указать название переменной. в таком случае оно будет вытянуто из левой части (перед '=>') и переданно в правую часть (после '=>')
 
   println( "lastRecursive = " + lastRecursive(list) )
 
